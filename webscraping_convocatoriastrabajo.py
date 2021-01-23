67f2618497708e923a1b7fe091a13d720b494b6e
HaslerUNMSM <hasler.licapa@unmsm.edu.pe>
2021-01-23 18:12:06 -0500

    Insertando identificador unico

--- webscraping_convocatoriastrabajo.py
+++ webscraping_convocatoriastrabajo.py
@@ -24,55 +24,66 @@ def get_content(contents):
     return lista
 
 
+def encontrar_id_unico(cadena):
+    i = 0
+    indice = -1
+    while i < len(cadena):
+        if cadena[i] == "-":
+            indice = i
+        i = i+1
+    auxiliar = cadena[indice+1:len(cadena)-5]
+    return auxiliar
+
+
 def scraping_ofertas(con, url_principal, prefix_url, sufix_url, pagina_inicial, cant_paginas, cant_ofertas, id_carga):
     controller = Controller()
     lista_oferta = []
     i = 1
-    # for i in range(pagina_inicial, cant_paginas):
+
     for i in range(CONVOCATORIATRABAJO["WS_PAGINA_INICIAL"], CONVOCATORIATRABAJO["WS_PAGINAS"]):
 
-        # MODIFICADO-----------------------------
+        # Recuperar el link de la pagina
         url_pagina = prefix_url
-        #print("------------------for i in range(pagina_inicial, cant_paginas)-------------------")
-        # print(i)
 
+        # Acceder al link principal
         req = requests.get(url_pagina)
         soup = BeautifulSoup(req.text, "lxml")
 
+        # Limitar el web scraping al area del codigo html pertinente
         avisos = soup.findAll("section")[0].find(
             "div", {"class": "info-convocatoria"}).findAll("article", {"class": "puesto"})
 
+        # Listar las ofertas y buscar en cada una
         lista_oferta = []
         for el in avisos:
 
-            # el=el.find("article","puesto")
-
-            # print("-------avisos--------")
-            # print(len(avisos))
             oferta = {}
-            #cont = cont + 1
-            # if cant_ofertas is not None:
-            #    if cont > cant_ofertas:
-            #        break
+
             # Obtiene el link para poder ver el detalle de la oferta
             href = el.find("h3").find("a")['href']
             link = url_principal + "/" + href
-            # print(link)
+
+            # Se obtiene el identificador unico
+            id_unico = encontrar_id_unico(href)
+            print("El identificador unico de esta oferta es: " + id_unico)
+            oferta["id_anuncioempleo"] = id_unico
+
+            # Almacena el id de carga
             oferta["id_carga"] = id_carga
+
             # Almacena la url de la pagina
             oferta["url_pagina"] = url_pagina
+
             # Almacena la url de la oferta
             oferta["url"] = link
 
-            # MODIFICADO
-
+            # Almacena el puesto de la oferta
             oferta["puesto"] = el.find("h3").find("a").get_text()
-            #print("Puesto " + oferta["puesto"])
 
             infoEmpresa = el.find({"div": "puesto-cont"}
                                   ).find({"div": "puesto-det"}).find_all("p")
-            #empresa = infoEmpresa[0]
-            # print(empresa.get_text())
+
+            # Almacena el nombre de la empresa de la oferta
             if infoEmpresa[0] != None:
                 oferta["empresa"] = infoEmpresa[0].get_text()
             else:
@@ -81,6 +92,7 @@ def scraping_ofertas(con, url_principal, prefix_url, sufix_url, pagina_inicial, 
             lugar = infoEmpresa[1].get_text()
             lugarK = lugar.split("   ")
 
+            # Almacena el lugar de la oferta
             if lugarK != None:
                 oferta["lugar"] = lugarK[0]
             else:
@@ -101,25 +113,27 @@ def scraping_ofertas(con, url_principal, prefix_url, sufix_url, pagina_inicial, 
 
             if aviso_deta != None:
                 oferta["detalle"] = aviso_deta
-            print("-----  IMPRIMIENTO AVISO_DETA------")
-            print(aviso_deta)
+            # print("-----  IMPRIMIENTO AVISO_DETA------")
+            # print(aviso_deta)
 
             # print("-------------------------------------------------------------------")
-            print("-------------------------OFERTA HASTA AHORA------------------------")
-            print(oferta)
+            # print("-------------------------OFERTA HASTA AHORA------------------------")
+            # print(oferta)
+
+            # Se agrega la oferta a la lista de ofertas
             lista_oferta.append(oferta)
-            print("-------------------------------------------------------------------")
+
             # print("-------------------------------------------------------------------")
+            # Se registra la oferta en la base de datos.
             id_Oferta = controller.registrar_oferta(con, oferta)
-            print("id de la oferta: ", id_Oferta)
-            aviso_deta_aux = aviso_deta
-            # print(aviso_deta)
+            print("\nid de la oferta: ", id_Oferta)
+
             # print("-------------------------------------------------------------------")
-            # print("-------------------------------------------------------------------")
+            # Se guarda linea por linea la informacion de la oferta
             arreglo = str(aviso_deta).splitlines()
-            #print("----------------------ANTES DE TRATAR-------------------------------")
-            # print(arreglo)
-            print("---------------------DESPUES DE TRATAR------------------------------")
+
+            # Se limpia la informacion obtenida
+            # print("---------------------DESPUES DE TRATAR------------------------------")
             for k in arreglo:
                 if arreglo.count(k) > 1:
                     arreglo.pop(arreglo.index(k))
@@ -130,7 +144,7 @@ def scraping_ofertas(con, url_principal, prefix_url, sufix_url, pagina_inicial, 
                 tamanio = len(arreglo)
                 arreglo.pop(tamanio-1)
                 k = k+1
-            print(arreglo)
+            # print(arreglo)
 
             tuplas = []
             for elemento in arreglo:
